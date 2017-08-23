547df67c75175001c294320d5b8fac8f00cdaf60
Fabian Vilers <fabian.vilers@dev-one.com>
2017-08-23 20:22:49 +0200

    Add Qobuz constructor

---
+++ test/qobuz.spec.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const chai = require('chai');
+const Qobuz = require('../lib');
+
+chai.should();
+
+describe('Qobuz', function () {
+  describe('ctor', function () {
+    function ctor () {
+      new Qobuz(null);
+    }
+
+    it('should throw an error for undefined application ID', function (done) {
+      ctor.should.throw('Client requires an application ID.');
+      done();
+    });
+
+    it('should set the application ID', function (done) {
+      const appId = '18ea6204-882f-11e7-bb31-be2e44b06b34';
+      const client = new Qobuz(appId);
+
+      client.appId.should.be.equal(appId);
+      done();
+    });
+
+    it('should set the application ID from the environment variable', function (done) {
+      const appId = '18ea6204-882f-11e7-bb31-be2e44b06b34';
+      process.env.QOBUZ_APP_ID = appId;
+
+      const client = new Qobuz();
+      process.env.QOBUZ_APP_ID = undefined;
+
+      client.appId.should.be.equal(appId);
+      done();
+    });
+  });
+});
