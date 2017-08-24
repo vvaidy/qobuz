981b459f33292f5150974a35c3db169570f628d6
Fabian Vilers <fabian.vilers@dev-one.com>
2017-08-24 12:57:28 +0200

    Add the flag method

--- test/comment/delete.spec.js
+++ test/comment/delete.spec.js
@@ -28,7 +28,7 @@ describe('Qobuz', function () {
 
       it('should return success in JSON', function (done) {
         const client = new Qobuz(appId);
-        const expected = require('./delete.json');
+        const expected = require('./success.json');
         const stub = sinon.stub(request, 'get').callsFake((options, callback) => callback(null, null, expected));
 
         client.comment.delete('MyAuThTokEn', '123456').should.eventually.deep.equal(expected).and.notify((err) => {
